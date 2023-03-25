f611c5f311c46bf1b99a1c6a55b3bb7a3f1c4177
ZAHID HUSSAIN KHAN <erzahidkhan101@gmail.com>
2023-03-25 16:35:35 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your goal here"
+          style={styles.textInput}
+        />
+        <Button title="Add Goal" style={styles.btn} />
+      </View>
+      <View style={styles.list}>
+        <Text>Your goals...</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    // alignContent: 'center',
+    padding: 50,
+    height: '100%',
+    width: '100%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 10,
+    padding: 5,
+  },
+  btn: {
+    marginLeft: 5,
+    width: '10%',
+    color: 'black',
   },
 });
+
+export default App;
