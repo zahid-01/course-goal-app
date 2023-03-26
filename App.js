00372f3a6d3d64b7721203ad2c55b1b75d4fd742
zahid-01 <erzahidkhan101@gmail.com>
2023-03-26 11:11:01 +0530

    Scrollable

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 const App = () => {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInpHandler = (e) => {
+    setEnteredGoal(e);
+  };
+
+  const addGoalHandler = () => {
+    if (enteredGoal.trim().length === 0) {
+      alert('Enter a valid goal');
+      return;
+    }
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoal,
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Enter your goal here"
           style={styles.textInput}
+          onChangeText={goalInpHandler}
         />
-        <Button title="Add Goal" style={styles.btn} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
+
       <View style={styles.list}>
-        <Text>Your goals...</Text>
+        <ScrollView>
+          {courseGoals.map((el) => (
+            <View style={styles.goal} key={Math.random()}>
+              <Text style={styles.goalText}>{el}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -21,26 +55,40 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    // alignContent: 'center',
-    padding: 50,
-    height: '100%',
-    width: '100%',
+    paddingTop: 70,
+    paddingHorizontal: 15,
+    flex: 1,
+    backgroundColor: '#d8e8e6',
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 10,
+    width: '70%',
+    marginRight: 2,
     padding: 5,
   },
-  btn: {
-    marginLeft: 5,
-    width: '10%',
-    color: 'black',
+  list: {
+    flex: 5,
+  },
+  goal: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white',
+    alignItems: 'center',
+  },
+  goalText: {
+    color: 'white',
   },
 });
 
